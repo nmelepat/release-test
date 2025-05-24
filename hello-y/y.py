9999d970ca9e98d5b7c12380f11ce66e4a800099
Nishanth Melepat <nishanth.melepat@amadeus.com>
2025-05-24 20:33:49 +0530

    feat: remove date feature

--- hello-y/y.py
+++ hello-y/y.py
@@ -1,8 +1,6 @@
-from datetime import datetime
-
 def greet():
     print("Hello world, I'm y")
-    print(f"Current date and time: {datetime.now()}")
+    
 
 if __name__ == "__main__":
     greet()
