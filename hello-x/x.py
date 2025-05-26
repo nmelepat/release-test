2e77ec187b618e01e7c798d75c32005fa303a4f9
Nishanth Melepat <nishanth.melepat@amadeus.com>
2025-05-26 22:22:23 +0530

    initial commit

---
+++ hello-x/x.py
@@ -0,0 +1,6 @@
+def greet():
+   
+    print(f"Hello world, I'm x")
+     
+if __name__ == "__main__":
+    greet()
---
+++ hello-y/y.py
@@ -0,0 +1,6 @@
+def greet():
+    print("Hello world, I'm y")
+    
+
+if __name__ == "__main__":
+    greet()
