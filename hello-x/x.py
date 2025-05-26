6ae870e02606375286c7abf0f8cfd92261467202
Nishanth Melepat <nishanth.melepat@amadeus.com>
2025-05-26 23:04:35 +0530

    feat: add date

--- hello-x/x.py
+++ hello-x/x.py
@@ -1,7 +1,8 @@
 import datetime
 
 def greet():
-    print(f"Hello world, I'm x")
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    print(f"Hello world, I'm x - Today is {current_date}")
      
 if __name__ == "__main__":
     greet()
