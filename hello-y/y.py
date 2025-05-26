e965cd73e4ad6df71dff3ff0f958d15595170acd
Nishanth Melepat <nishanth.melepat@amadeus.com>
2025-05-26 21:43:08 +0530

    initial commit

---
+++ hello-y/y.py
@@ -0,0 +1,9 @@
+def greet():
+    print("Hello world, I'm y")
+    
+
+if __name__ == "__main__":
+    greet()
+
+# It defines a function `greet` that prints a greeting message.
+# The function is called when the script is run directly.
